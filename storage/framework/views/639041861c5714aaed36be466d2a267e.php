b63993998f4c0023370895a667bd87e68138c8b1
Budi Gunawan <gunawan830@gmail.com>
2026-04-09 19:58:30 +0700

    Fix .gitignore and track Laravel source files

--- storage/framework/views/639041861c5714aaed36be466d2a267e.php
+++
@@ -1,4 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-indigo-600 dark:bg-indigo-500 border border-transparent rounded-lg font-bold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 dark:hover:bg-indigo-600 focus:bg-indigo-700 dark:focus:bg-indigo-600 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 shadow-md hover:shadow-lg transition ease-in-out duration-300'])); ?>>
-    <?php echo e($slot); ?>
-
-</button><?php /**PATH D:\01 BEGE\99 DEVS\LARAVEL\SKNET\SKNET-CRM-NEW\01\resources\views/components/button.blade.php ENDPATH**/ ?>
